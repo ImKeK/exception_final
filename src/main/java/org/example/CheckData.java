fb5eb1005d64cb3bc1b89ff8a54a0a6e68c84ff2
Egor <egorkiper21@gmail.com>
2023-09-29 18:47:39 +0300

    1

---
+++ src/main/java/org/example/CheckData.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class CheckData {
+    /**
+     * Провеяем правильное количество введенных данных
+     * @param strArray Массив из строки, введенной в консоль.
+     */
+    public static void checkArrayLength(String[] strArray) {
+        if (strArray.length < 6) {
+            throw new RuntimeException("Количество введенных данных меньше, чем необходимо");
+        } else if (strArray.length > 6) {
+            throw new RuntimeException("Количество введенных данных больше, чем необходимо");
+        }
+    }
+    public static void checkDateBirth(String date){
+
+    }
+
+    /**
+     * Проверям на правильность введение пола(M/Ж)
+     */
+    public static void checkGender(String c){
+        if (!c.equals("m") && !c.equals("f"))
+            throw new RuntimeException("Пол указан неверно");
+    }
+}
